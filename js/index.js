ba8d54a64d5cf7fb1a47e89253f6b1e12473da3c
Fabio <fabiocostazucco@gmail.com>
2019-08-07 20:04:47 -0300

    Exercício aonde gera-se números aleatórios de 1 a 50 para escolher numeros aleatórios de 1000 a 1999, que dentro de um array, apareceram se forem divididos por 11 com resto 5.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
 document.body.append("Os são que divididos por 11 dão resto 5 são: ")
+var list = [];
 
-for (var i = 1000; i < 1999; i++) {
-	if (i%11===5) {
-		document.body.append(i +", ")
+var aleat = Math.floor(Math.random() * 50)+1;
+
+for (var i = 0; i < aleat; i++) {
+	var number = Math.floor(Math.random() * 999) + 1000;
+	if (number%11===5) {
+		list.push(number);
 	}
 }
+
+if (list.length === 0) {
+		document.body.append("Nenhum número divido por 11 de resto 5")
+	}else {
+		document.body.append(list.join(','))	
+}
+
+
+
